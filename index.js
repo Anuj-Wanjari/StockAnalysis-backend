cd44ecbee65e5418f508e3acf9716f9407da93ed
Anuj-Wanjari <anujwanjari5@gmail.com>
2024-08-20 01:19:48 +0530

    connection

--- index.js
+++ index.js
@@ -6,20 +6,25 @@ const Stock = require('./models/stock'); // Import the Stock model
 
 const app = express();
 app.use(cors({
-    origin: "https://stock-analysis-frontend-iota.vercel.app",
-    credentials: true
+    origin: "https://stock-analysis-frontend-iota.vercel.app"
 }));
 app.use(express.json());
 
+async function connectMongoDB(){
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://anujwanjari5:RHEKPqcROl2Wh8hj@cluster0.vuoxk.mongodb.net/', {
+return mongoose.connect('mongodb+srv://anujwanjari5:RHEKPqcROl2Wh8hj@cluster0.vuoxk.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+}).then((res) => {
     console.log('Database connected successfully');
+    return true;
 }).catch(err => {
     console.error('Database connection error:', err);
+    return false;
 });
+
+}
+
 
 // Define a route to send sample data
 app.get('/api/stocks/nifty50', async (req, res) => {
@@ -57,7 +62,16 @@ app.get('/api/stocks/niftysmallcap', async (req, res) => {
     }
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
 
+async function startServer(){
+    // Connect to MongoDB
+    await connectMongoDB();
+
+    // Start the server
+    app.listen(5000, () => {
+        console.log('Server is running on port 5000');
+    });
+    
+}
+
+startServer();
